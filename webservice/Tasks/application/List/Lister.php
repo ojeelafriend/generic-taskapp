70c35189e26c7e20ab450ce6da78869207f8e7b2
odev <ts8irolon@gmail.com>
2022-05-26 11:00:37 -0300

    Creo el lister y su excepción, controlando los errores y aplicando el read de infraestructura

---
+++ webservice/Tasks/application/List/Lister.php
@@ -0,0 +1,27 @@
+<?php
+    require_once(__DIR__ . '../../../domain/IRepository.php');
+
+    class Lister{
+        private $repository;
+
+        public function __construct(\IRepository $repository){
+            $this->repository = $repository;
+        }
+
+        public function run(int $initial, int $items){
+            if($initial == null || $items == null){
+                throw new ListException('Number of items or initial have null values');
+            }
+
+            $tasks = $this->repository->read($initial, $items);
+
+            if(!$tasks){
+                throw new ListException('Tasks dont exist, check initial or requested items');
+            }
+
+            return $tasks;
+
+        }
+    }
+
+?>

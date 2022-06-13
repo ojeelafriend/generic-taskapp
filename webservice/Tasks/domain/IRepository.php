e594353ef6f3cad09b8addef64d9848efc16f598
odev <ts8irolon@gmail.com>
2022-06-13 16:46:21 -0300

    añado al dominio el metodo delete al repository

--- webservice/Tasks/domain/IRepository.php
+++ webservice/Tasks/domain/IRepository.php
@@ -3,5 +3,6 @@
         public function save(\Task $task);
         public function read(int $initial, int $indexPage);
         public function checkRows();
+        public function delete(int $taskId);
     }
 ?>

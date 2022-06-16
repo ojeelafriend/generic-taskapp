e86af8c17fa8a989bb3be148efe6bb9f0ac0102b
odev <ts8irolon@gmail.com>
2022-06-16 01:28:50 -0300

    Implemento los onclicks generados en los componentes y taskapp.html

---
+++ public/js/Tasks/events/submit.js
@@ -0,0 +1,19 @@
+document.addEventListener("submit", async (evt) => {
+  evt.preventDefault();
+});
+
+async function submitSendTask() {
+  await Notifier.send(new FormData(document.getElementById("todo-form")));
+  await submitPage(1);
+  await PageNumber.render();
+}
+
+async function submitPage(pageNumber) {
+  await TaskComponent.render(CurrentPage.setPage(pageNumber));
+}
+
+async function submitRemoveTask(itemId) {
+  await Notifier.remove(itemId);
+  await submitPage(CurrentPage.getPage());
+  await PageNumber.render();
+}
